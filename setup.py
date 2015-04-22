b745b72177505500b82011b98dff31303de9813f
Andrew W. Nystrom <anystrom@Contatas-MacBook-Pro.local>
2015-04-22 00:16:34 -0500

    upticked version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "disk_dict",
     packages = find_packages(),
-    version = "2.0.1",
+    version = "2.0.2",
     description = "A disk-based key/value store in Python.",
     author = "Andrew Nystrom",
     author_email = "AWNystrom@gmail.com",
